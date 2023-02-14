407db96667a74909108ff3c0dd10919f13675b23
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-14 14:57:21 +0530

    added: maximum Float value using Generics

---
+++ Generics_prob.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_problems
+{
+    internal class Generics_prob
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ namespace Generics_problems
             Console.WriteLine(" Welcome to Maximum Problem using Generics");
 
             int a = 7, b = 6, c = 4;
+            float p = 6.7f, q = 8.7f, r = 5.5f;
+
             Intmaxvalue(a, b, c);
+            Floatmax(p, q, r);
         }
 
 
@@ -24,8 +27,23 @@ namespace Generics_problems
             {
                 max= c;
             }
-            Console.WriteLine("Maximum Integer is: " + max);
+            Console.WriteLine("Maximum Integer Value is: " + max);
 
+        }
+        public static void Floatmax(float a,float b, float c)
+        {
+            float max = a;
+            {
+                if(b.CompareTo(a) > 0)
+                {
+                    max= b;
+                }
+                if(c.CompareTo(b) > 0)
+                {
+                    max= c;
+                }
+                Console.WriteLine("Maximum Float is Value is: " + max);
+            }
         }
     }
 }
