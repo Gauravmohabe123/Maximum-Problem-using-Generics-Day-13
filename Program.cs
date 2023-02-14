acc5e94e53f187881cc49887b6f6dc488e0ec197
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-14 15:08:45 +0530

    added: maximum string value using Generics

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Generics_problems
 {
@@ -10,9 +11,11 @@ namespace Generics_problems
 
             int a = 7, b = 6, c = 4;
             float p = 6.7f, q = 8.7f, r = 5.5f;
+            string l = "Apple", m = "Peach", n = "Banana";
 
             Intmaxvalue(a, b, c);
             Floatmax(p, q, r);
+            MaxString(l, m, n);
         }
 
 
@@ -44,6 +47,20 @@ namespace Generics_problems
                 }
                 Console.WriteLine("Maximum Float is Value is: " + max);
             }
+            
+        }
+        public static void MaxString(string l,string m, string n)
+        {
+            string max=l;
+            if (m.CompareTo(l) > 0)
+            {
+                max= m;
+            }
+            if (n.CompareTo(m) > 0)
+            {
+                max= n;
+            }
+            Console.WriteLine("Enter maximum string: " + max);
         }
     }
 }
