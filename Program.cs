a248e9d2b798947ba37897e3a75194b1086ec532
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-14 14:23:53 +0530

    added:Generics Program Problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Maximum Problem using Generics");
+        }
+    }
+}
