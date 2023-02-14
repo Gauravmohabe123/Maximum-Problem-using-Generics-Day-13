c9db659ad34118eb3ba9005b6138717d1a13e7cf
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-14 14:39:13 +0530

    added: maximum integer using Generics

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace Generics_problems
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Maximum Problem using Generics");
+
+            int a = 7, b = 6, c = 4;
+            Intmaxvalue(a, b, c);
+        }
+
+
+        public static void Intmaxvalue(int a,int b, int c)
+        {
+            int max = a;
+            if(b.CompareTo(a) >0)
+            {
+                max= b;
+            }
+            if(c.CompareTo(b) >0)
+            {
+                max= c;
+            }
+            Console.WriteLine("Maximum Integer is: " + max);
+
         }
     }
 }
